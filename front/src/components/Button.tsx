d495efef8009c9669cdaaa7972557e29c7a2343c
victor077 <victor.souza@bandtec.com.br>
2023-06-28 23:30:32 -0300

    melhorando Button layout

--- front/src/components/Button.tsx
+++ front/src/components/Button.tsx
@@ -7,15 +7,15 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const Button = ({ children, variant, ...buttonProps }: ButtonProps) => {
-  const varinat_primary =
-    "bg-slate-50 text-base text-zinc-900 py-2 px-4 border rounded-xl shadow-md shadow-zinc-600 font-semibold tracking-wide";
-  const variant_secondary =
-    "bg-zinc-900 text-base text-slate-100 py-2 px-4 border rounded-xl shadow-lg shadow-slate-200 font-semibold tracking-wide";
+  const varinat_bg_light =
+    "bg-slate-200 hover:bg-slate-100 :bg-slate-100  text-base text-zinc-900 py-2 px-4 border rounded-xl shadow-sm shadow-zinc-600 font-semibold tracking-wide";
+  const variant_bg_dark =
+    "bg-zinc-900 hover:bg-zinc-800 text-base text-slate-100 py-2 px-4 border rounded-xl shadow-lg shadow-slate-200 font-semibold tracking-wide";
 
   return (
     <button
       {...buttonProps}
-      className={variant === "primary" ? varinat_primary : variant_secondary}
+      className={variant === "primary" ? varinat_bg_light : variant_bg_dark}
     >
       {children}
     </button>
