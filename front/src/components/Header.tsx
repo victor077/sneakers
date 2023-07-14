db94cf0053e740a53549b38f2fdaca1f069b7011
victor077 <victor.souza@federalconsig.com.br>
2023-07-14 17:31:13 -0300

    alterando header

---
+++ front/src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import Logo from "./Logo";
+
+function Header() {
+  return (
+    <>
+      <header className=" bg-[#313132] flex  justify-center items-center">
+        <nav className="w-4/5 py-4">
+          <Logo />
+        </nav>
+      </header>
+      <header>
+        <nav className="bg-[#F5F5F7] justify-center items-center flex">
+          <ul className="flex gap-8 cursor-pointer py-6 font-medium text-lg">
+            <Link href="/login">
+              <li>Login</li>
+            </Link>
+            <li>Comunidade</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
