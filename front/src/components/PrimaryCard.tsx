5f178275adb107d2c2e6439daece53d551a30740
victor077 <victor.souza@federalconsig.com.br>
2023-07-14 17:33:16 -0300

    criando componente car

---
+++ front/src/components/PrimaryCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PrimaryCard = () => {
+  return (
+    <div className="flex justify-center items-center h-screen" >
+        <div className="w-3/4 border rounded-3xl flex justify-between bg-gray-50">
+            teste
+        </div>
+    </div>
+  )
+}
+
+export default PrimaryCard
