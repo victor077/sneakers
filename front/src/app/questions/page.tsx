bcf8e80e5544d8fdb61404d2ddc884343ef31bf4
victor077 <victor.souza@bandtec.com.br>
2023-07-17 21:30:24 -0300

    Criando tela de questions

---
+++ front/src/app/questions/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+import Card from "@/components/Card";
+// import { useRouter } from "next/router";
+
+const Page = () => {
+  // const router = useRouter();
+
+  return (
+      <div className=" h-96 flex items-center justify-center">
+        <Card />
+      </div>
+  );
+};
+
+export default Page;
