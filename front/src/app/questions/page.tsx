e59985f7c3632367be787aec812eac8b2f713d6f
victor077 <victor.souza@bandtec.com.br>
2023-11-22 18:01:32 -0300

    adicionando logica para as question

--- front/src/app/questions/page.tsx
+++ front/src/app/questions/page.tsx
@@ -1,17 +1,31 @@
 "use client";
 import React from "react";
 
-import Card from "@/components/Card";
-import Button from "@/components/Button";
+import CardQuestion from "./components/cardQuestion";
 
 const Page = () => {
+  const listQuestions = [
+    {
+      id: 1,
+      question: "Quais sites você costuma acessar para pesquisar sobre seus tenis ?",
+      options: ['Adidas', 'Authentic feet', 'Nike', 'Outros']
+    },
+    {
+      id: 2,
+      question: "Pergunta dois ?",
+      options: ['Adidas', 'Authentic feet', 'Nike', 'Outros']
+    },
+    {
+      id: 3,
+      question: "Pergunta três",
+      options: ['Adidas', 'Authentic feet', 'Nike', 'Outros']
+    },
+  ]
   return (
     <div className=" h-96 flex items-center justify-center">
-      <Card
-        body={"teste"}
-        firstButton={<Button variant="primary">teste</Button>}
-        lastButton={<Button variant="secondary">teste</Button>}
-      ></Card>
+      <CardQuestion
+        list={listQuestions}
+      />
     </div>
   );
 };
