08d9dc02fad51a0d010c9efebeeb315bfab53af0
victor077 <victor.souza@bandtec.com.br>
2023-11-22 22:14:45 -0300

    melhorando componente para ficar mais escalavel

--- front/src/components/Button.tsx
+++ front/src/components/Button.tsx
@@ -1,23 +1,39 @@
-import React, { ButtonHTMLAttributes, ReactNode } from "react";
+import React, { ComponentProps } from "react";
+import { tv, VariantProps } from "tailwind-variants"
 
-type ButtonVarinat = "primary" | "secondary";
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant: ButtonVarinat;
-  children: ReactNode;
+const button = tv({
+  base: ' tracking-wide text-base border rounded-md shadow-sm font-semibold',
+  variants: {
+    size: {
+      default: 'h-10 w-24',
+      sm: 'h-8 px-3',
+      xs: 'h-6 px-2 text-xs',
+      full: "w-full h-10"
+    },
+    color: {
+      primary: 'bg-gray-800 hover:bg-gray-950 text-gray-100 shadow-gray-900 hover:shadow-gray-950 border-none',
+      outline: 'bg-transparent text-gray-900 tracking-wide text-base border border-zinc-400  hover:border-gray-950 rounded-md shadow-sm shadow-gray-900',
+      secondary: 'bg-gray-200 hover:bg-gray-100 text-gray-800 shadow-gray-900 hover:shadow-gray-950 border-none'
+    },
+  },
+  defaultVariants: {
+    size: "default",
+    color: "primary"
+  },
+})
+
+export type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button> & {
+
 }
 
-const Button = ({ children, variant, ...buttonProps }: ButtonProps) => {
-  const varinat_bg_light =
-    "bg-slate-200 hover:bg-slate-100 :bg-slate-100  text-base text-zinc-900 py-2 px-4 border rounded-xl shadow-sm shadow-zinc-600 font-semibold tracking-wide";
-  const variant_bg_dark =
-    "bg-zinc-900 hover:bg-zinc-800 text-base text-slate-100 py-2 px-4 border rounded-xl shadow-lg shadow-slate-200 font-semibold tracking-wide";
 
+const Button = ({ color, size, ...buttonProps }: ButtonProps) => {
   return (
     <button
       {...buttonProps}
-      className={variant === "primary" ? varinat_bg_light : variant_bg_dark}
+      className={button({ color, size })}
     >
-      {children}
+      {buttonProps.children}
     </button>
   );
 };
